4a514b801b4f7d6ca2105b33afea56cb49ca7b02
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-18 14:40:59 +0100

    feat: add basic styles and handle css request inside server

--- index.js
+++ index.js
@@ -2,32 +2,37 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-console.log('request')
-res.setHeader('Content-type', 'text/html')
-console.log(req.url)
-let path = `/`;
-switch(req.url){
-    case '/': 
-    path += `index.html`
-    break;
-    case '/about':
-    path += `about.html`
-    break;
-    case '/contact-me':
-    path += `contact-me`;
-    break;
-    default: 
-    path += '404.html';
-    break;
-}
-fs.readFile(`./${path}`, (err, data) => {
-    if(err) {
-        console.log(err)
-    } else {
-        res.write(data)
-        res.end()
+    contentType = 'text/html'
+    let path = `/`;
+    let r = req.url
+    switch (true) {
+        case r === '/':
+            path += `index.html`
+            break;
+        case r === '/about':
+            path += `about.html`
+            break;
+        case r === '/contact-me':
+            path += `contact-me`;
+            break;
+        case r.endsWith(".css"):
+            path += 'styles.css';
+            contentType = 'text/css';
+            break;
+        default:
+            path += '404.html';
+            break;
     }
-})
+    res.setHeader('Content-type', contentType)
+    fs.readFile(`./${path}`, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.write(data)
+            res.end()
+        }
+    })
+   
 })
 server.listen(8000, 'localhost', () => {
     console.log('listen on the port 8000')
